7abbee9ef55d2244a9b98d70258bd7ecfffdc0e1
Marduk <randomlyricsmusic@gmail.com>
2020-11-15 20:02:28 +0100

    test

---
+++ src/CinemaSystem.Infrastructure/Database/MockDB/IMockDB.cs
@@ -0,0 +1,10 @@
+﻿namespace CinemaSystem.Infrastructure.Database.MockDB
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public interface IMockDB
+    {
+    }
+}
---
+++ src/CinemaSystem.Infrastructure/Database/MockDB/MockDB.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaSystem.Infrastructure.Database.MockDB
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class MockDB : IMockDB
+    {
+
+    }
+}
